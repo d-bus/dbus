907832e00849ca454322052981dbb122ea537506
Simon McVittie <smcv@collabora.com>
2018-10-03 17:51:35 +0100

    ci: Explicitly install cmake
    Travis-CI workers have cmake preinstalled, but Gitlab-CI Docker images
    typically don't.
    
    Signed-off-by: Simon McVittie <smcv@collabora.com>
    Bug: https://bugs.freedesktop.org/show_bug.cgi?id=108177
    Acked-by: Philip Withnall <withnall@endlessm.com>

--- tools/ci-install.sh
+++ tools/ci-install.sh
@@ -114,6 +114,7 @@ case "$ci_distro" in
             autoconf-archive \
             automake \
             autotools-dev \
+            cmake \
             debhelper \
             dh-autoreconf \
             dh-exec \
