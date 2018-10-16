195c843b7b6f77819d233460d584672ff16fae93
Ralf Habacker <ralf.habacker@freenet.de>
2018-10-16 13:59:04 +0200

    cmake: Add comment to configure checks explaining why we are not specifying HAVE_SOCKLEN_T
    Bug: https://gitlab.freedesktop.org/dbus/dbus/issues/117
    Reviewed-by: Simon McVittie <smcv@collabora.com>

--- cmake/ConfigureChecks.cmake
+++ cmake/ConfigureChecks.cmake
@@ -78,7 +78,7 @@ check_type_size("long"      SIZEOF_LONG)
 check_type_size("long long" SIZEOF_LONG_LONG)
 check_type_size("__int64"   SIZEOF___INT64)
 set(CMAKE_EXTRA_INCLUDE_FILES "sys/socket.h")
-check_type_size("socklen_t" SOCKLEN_T)          #  dbus-sysdeps-unix.c
+check_type_size("socklen_t" SOCKLEN_T) # define HAVE_SOCKLEN_T and SOCKLEN_T for size
 set(CMAKE_EXTRA_INCLUDE_FILES)
 
 # DBUS_INT64_TYPE
