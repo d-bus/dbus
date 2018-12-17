5424eb2b0d93fb60cee658e1f9198f43dfd15ec7
Simon McVittie <smcv@collabora.com>
2018-12-17 11:34:41 +0000

    dbus-marshal-*-util: Remove all trailing whitespace
    This is in preparation for moving the files: our commit hook would
    object to the delete + add otherwise.
    
    Signed-off-by: Simon McVittie <smcv@collabora.com>

--- dbus/dbus-marshal-byteswap-util.c
+++ dbus/dbus-marshal-byteswap-util.c
@@ -41,7 +41,7 @@ do_byteswap_test (int byte_order)
     _dbus_test_fatal ("oom");
 
   opposite_order = byte_order == DBUS_LITTLE_ENDIAN ? DBUS_BIG_ENDIAN : DBUS_LITTLE_ENDIAN;
-  
+
   sequence = 0;
   while (_dbus_test_generate_bodies (sequence, byte_order, &signature, &body))
     {
@@ -64,7 +64,7 @@ do_byteswap_test (int byte_order)
                               &body, 0);
       _dbus_type_reader_init (&copy_reader, opposite_order, &signature, 0,
                               &copy, 0);
-      
+
       if (!_dbus_type_reader_equal_values (&body_reader, &copy_reader))
         {
           _dbus_verbose_bytes_of_string (&signature, 0,
@@ -76,9 +76,9 @@ do_byteswap_test (int byte_order)
 
           _dbus_test_fatal ("Byte-swapped data did not have same values as original data");
         }
-      
+
       _dbus_string_free (&copy);
-      
+
       _dbus_string_set_length (&signature, 0);
       _dbus_string_set_length (&body, 0);
       ++sequence;
--- dbus/dbus-marshal-validate-util.c
+++ dbus/dbus-marshal-validate-util.c
@@ -433,7 +433,7 @@ _dbus_marshal_validate_test (const char *test_data_dir _DBUS_GNUC_UNUSED)
   _dbus_string_free (&str);
 
   /* Body validation; test basic validation of valid bodies for both endian */
-  
+
   {
     int sequence;
     DBusString signature;
@@ -465,7 +465,7 @@ _dbus_marshal_validate_test (const char *test_data_dir _DBUS_GNUC_UNUSED)
         _dbus_string_set_length (&body, 0);
         ++sequence;
       }
-                                                     
+
     sequence = 0;
     while (_dbus_test_generate_bodies (sequence, DBUS_BIG_ENDIAN,
                                        &signature, &body))
@@ -493,7 +493,7 @@ _dbus_marshal_validate_test (const char *test_data_dir _DBUS_GNUC_UNUSED)
     _dbus_string_free (&signature);
     _dbus_string_free (&body);
   }
-  
+
   return TRUE;
 }
 
